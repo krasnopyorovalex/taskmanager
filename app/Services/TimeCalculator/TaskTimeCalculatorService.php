3e9d11e3028841ef975d72060aadcad1b3f714aa
Sanek <sanek@localhost.localdomain>
2021-03-27 18:15:24 +0300

    change COST_PER_SECOND

--- app/Services/TimeCalculator/TaskTimeCalculatorService.php
+++ app/Services/TimeCalculator/TaskTimeCalculatorService.php
@@ -13,7 +13,7 @@ use Illuminate\Database\Eloquent\Collection;
  */
 class TaskTimeCalculatorService extends AbstractTimeCalculatorService
 {
-    private const COST_PER_SECOND = 0.2084;
+    private const COST_PER_SECOND = 0.20833334;
 
     public function total(Collection $collection): int
     {
